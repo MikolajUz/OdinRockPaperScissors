e358f9061007c7b1e274e6d045caeccc9b4178be
MikeTheStrike <muzialko@gmail.com>
2023-03-28 12:00:43 +0200

    one round game done

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 function getComputerChoice(){
-    
+
      switch (Math.floor(Math.random() * 3)){ //random number from 0 to 2  
         case 0:
-        console.log('Rock');
+        return 'Rock';
             break;
             case 1:
-            console.log('Paper');
-                break;
+            return 'Paper';
+            break;
                 case 2:
-                console.log('Scissors');
+                return 'Scissors';
                 break;
                 }
 }
-console.log(getComputerChoice());
+
+function gameRound(plrSel,compSel){
+
+    compSel=getComputerChoice();
+    plrSel=plrSel.toUpperCase();
+    compSel=compSel.toUpperCase();
+
+    if (plrSel===compSel){
+        return 'Tie';
+    }
+        else
+        if((plrSel==="ROCK" && compSel==="SCISSORS") || (plrSel==="SCISSORS" && compSel==="PAPER") || (plrSel==="PAPER" && compSel==="ROCK"))
+            return 'You won '+plrSel+ ' beats '+compSel;
+        
+        else
+            if((plrSel==="ROCK" && compSel==="PAPER") || (plrSel==="SCISSORS" && compSel==="ROCK") || (plrSel==="PAPER" && compSel==="SCISSORS"))
+            return 'You lose '+compSel+ ' beats '+plrSel;
+            
+    }
+    
+
+console.log(gameRound('PapEr',));
+
     
 
 
