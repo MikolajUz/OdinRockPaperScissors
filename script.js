e8596c5c2149a90ffd376a5f98613d6b1bdcbf84
MikeTheStrike <muzialko@gmail.com>
2023-03-28 13:12:36 +0200

    game done, player input left to do

--- script.js
+++ script.js
@@ -31,10 +31,42 @@ function gameRound(plrSel,compSel){
             return 'You lose '+compSel+ ' beats '+plrSel;
             
     }
+function winnerCheck(plr,comp)
+{
+    if (plr==comp){
+        return ' human and computer(tie)';
+    }
+    else {if(plr>comp){
+        return 'human';
+    }
+        else{
+            return 'computer';
+        }
+    }
+}
+
+
+function game(){
+    let plrscore=0;
+    let compscore=0;
     
+    for (let i=0;i<5;i++){
+        let roundscore=gameRound('Rock',);
+        console.log(roundscore);
+        if (roundscore.slice(0,7)==='You won') plrscore++;
+        if (roundscore.slice(0,8)==='You lose') compscore++;
+    }
+    winner=winnerCheck(plrscore,compscore);
+    plrscore=plrscore.toString();
+    compscore=compscore.toString();
+    
+    let score='Human scored '+plrscore+' and computer scored '+compscore+' .The winner is '+winner ;
+        return score;
+        
+    
+}
 
-console.log(gameRound('PapEr',));
-
+console.log(game());
     
 
 
